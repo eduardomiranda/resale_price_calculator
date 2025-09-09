af2f2b6deacfe43b9f68c5d3af22f12b5cb0712f
Eduardo Miranda <eduardo.dpm@gmail.com>
2025-09-09 13:27:46 -0300

    Correção de escape R\$

--- domain/pricing.py
+++ domain/pricing.py
@@ -64,9 +64,9 @@ def calculate_sale_price(
             sale_price_txt += f'''\n\n'''
             sale_price_txt += f'''\n\n**Preço de venda** = *custo de compra* × *multiplicador líquido* + (*custo de compra* × *multiplicador líquido* × *margem de venda*)'''
             sale_price_txt += f'''\n\n**Preço de venda** = *custo de compra* × *multiplicador líquido* × (1 + *margem de venda*)'''
-            sale_price_txt += f'''\n\n**🏷️ Preço de venda** = R\$ {purchase_price:,.2f} × {net_multiplier:,.5f} × (1 + {profit_rate*100:,.2f}%) = **R$ {sale_price:,.2f}**'''
-            sale_price_txt += f'''\n\n**💸 Impostos** = R\$ {sale_price * tax_rate:,.2f}'''
-            sale_price_txt += f'''\n\n**💰 Margem bruta** = R\$ {net_profit:,.2f}'''
+            sale_price_txt += f'''\n\n**🏷️ Preço de venda** = R\\$ {purchase_price:,.2f} × {net_multiplier:,.5f} × (1 + {profit_rate*100:,.2f}%) = **R$ {sale_price:,.2f}**'''
+            sale_price_txt += f'''\n\n**💸 Impostos** = R\\$ {sale_price * tax_rate:,.2f}'''
+            sale_price_txt += f'''\n\n**💰 Margem bruta** = R\\$ {net_profit:,.2f}'''
 
         elif profit_application == "Aplicado na venda":
             sale_denominator = 1 - profit_rate - tax_rate
@@ -79,9 +79,9 @@ def calculate_sale_price(
             sale_price_txt += f'''\n\n**denominador de venda** = 100% - {profit_rate*100:,.2f}% -  {tax_rate * 100:,.2f}% = {sale_denominator*100:,.2f}%'''
             sale_price_txt += f'''\n\n'''
             sale_price_txt += f'''\n\n**Preço de venda** = *custo de compra* ÷ *denominador de venda*'''
-            sale_price_txt += f'''\n\n**🏷️ Preço de venda** = R\$ {purchase_price:,.2f} ÷ {sale_denominator:,.4f} = **R$ {sale_price:,.2f}**'''
-            sale_price_txt += f'''\n\n**💸 Impostos** = R\$ {sale_price * tax_rate:,.2f}'''
-            sale_price_txt += f'''\n\n**💰 Margem bruta** = R\$ {net_profit:,.2f}'''
+            sale_price_txt += f'''\n\n**🏷️ Preço de venda** = R\\$ {purchase_price:,.2f} ÷ {sale_denominator:,.4f} = **R$ {sale_price:,.2f}**'''
+            sale_price_txt += f'''\n\n**💸 Impostos** = R\\$ {sale_price * tax_rate:,.2f}'''
+            sale_price_txt += f'''\n\n**💰 Margem bruta** = R\\$ {net_profit:,.2f}'''
 
         else:
             raise ValueError(f"Invalid profit_application: {profit_application}")
@@ -103,11 +103,11 @@ def calculate_sale_price(
             sale_price_txt += f'''\n\n**preço de venda anual** = *preço de compra* × *multiplicador líquido* × (100% + *margem de venda* + *taxa de juros*)'''
             # sale_price_txt += f'''\n\n**preço de venda mensal** = *preço de venda anual* ÷ 12'''
             sale_price_txt += f'''\n\n'''
-            sale_price_txt += f'''\n\n**🏷️ Preço de venda anual** = R\$ {purchase_price:,.2f}  × {net_multiplier:,.5f} × (100% + {profit_rate*100:,.2f}% + {interest_rate*100:,.2f}%) = **R$ {annual_sale_price:,.2f}**'''
+            sale_price_txt += f'''\n\n**🏷️ Preço de venda anual** = R\\$ {purchase_price:,.2f}  × {net_multiplier:,.5f} × (100% + {profit_rate*100:,.2f}% + {interest_rate*100:,.2f}%) = **R$ {annual_sale_price:,.2f}**'''
             sale_price_txt += f'''\n\n**🏷️ Preço de venda mensal** =  {annual_sale_price:,.2f} ÷ 12 = **R$ {sale_price:,.2f}**'''
-            sale_price_txt += f'''\n\n**💸 Impostos** = R\$ {annual_sale_price * tax_rate:,.2f}'''
-            sale_price_txt += f'''\n\n**🏦 Juros** = R\$ {purchase_price * interest_rate:,.2f}'''
-            sale_price_txt += f'''\n\n**💰 Margem bruta** = R\$ {net_profit:,.2f}'''
+            sale_price_txt += f'''\n\n**💸 Impostos** = R\\$ {annual_sale_price * tax_rate:,.2f}'''
+            sale_price_txt += f'''\n\n**🏦 Juros** = R\\$ {purchase_price * interest_rate:,.2f}'''
+            sale_price_txt += f'''\n\n**💰 Margem bruta** = R\\$ {net_profit:,.2f}'''
 
         elif profit_application == "Aplicado na venda":
             sale_denominator = 1 - profit_rate - tax_rate
@@ -124,11 +124,11 @@ def calculate_sale_price(
             sale_price_txt += f'''\n\n**multiplicador líquido** = 1 ÷ (100% - {tax_rate*100:,.2f}%) = {net_multiplier:,.5f}'''
             sale_price_txt += f'''\n\n**preço de venda anual** = (*preço de compra* ÷ *denominador de venda*) + (*preço de compra* × *taxa de juros* × *multiplicador líquido*)'''
             # sale_price_txt += f'''\n\n**preço de venda mensal** = *preço de venda anual* ÷ 12'''
-            sale_price_txt += f'''\n\n**🏷️ Preço de venda anual** = (R\$ {purchase_price:,.2f} ÷ {sale_denominator:,.4f}) + (R\$ {purchase_price:,.2f} × {interest_rate*100:,.2f}% × {net_multiplier:,.5f}) = **R$ {annual_sale_price:,.2f}**'''
+            sale_price_txt += f'''\n\n**🏷️ Preço de venda anual** = (R\\$ {purchase_price:,.2f} ÷ {sale_denominator:,.4f}) + (R\\$ {purchase_price:,.2f} × {interest_rate*100:,.2f}% × {net_multiplier:,.5f}) = **R$ {annual_sale_price:,.2f}**'''
             sale_price_txt += f'''\n\n**🏷️ Preço de venda mensal** =  {annual_sale_price:,.2f} ÷ 12 = **R$ {sale_price:,.2f}**'''
-            sale_price_txt += f'''\n\n**💸 Impostos** = R\$ {annual_sale_price * tax_rate:,.2f}'''
-            sale_price_txt += f'''\n\n**🏦 Juros** = R\$ {purchase_price * interest_rate:,.2f}'''
-            sale_price_txt += f'''\n\n**💰 Margem bruta** = R\$ {net_profit:,.2f}'''
+            sale_price_txt += f'''\n\n**💸 Impostos** = R\\$ {annual_sale_price * tax_rate:,.2f}'''
+            sale_price_txt += f'''\n\n**🏦 Juros** = R\\$ {purchase_price * interest_rate:,.2f}'''
+            sale_price_txt += f'''\n\n**💰 Margem bruta** = R\\$ {net_profit:,.2f}'''
 
         else:
             raise ValueError(f"Invalid profit_application: {profit_application}")
